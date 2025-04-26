aa0f511bf5648c5b513d9d66400d69cf051b4d6c
ahmadrezapahlevi <zare95752@gmail.com>
2025-04-26 18:58:21 +0700

    update

--- app/Filament/Resources/NewsResource.php
+++ app/Filament/Resources/NewsResource.php
@@ -44,6 +44,7 @@ class NewsResource extends Resource
                 Forms\Components\RichEditor::make('content')
                 ->required()
                 ->columnSpanFull(),
+                Forms\Components\Toggle::make('is_featured')
             ]);
     }
 
@@ -56,6 +57,7 @@ class NewsResource extends Resource
                 Tables\Columns\TextColumn::make('title'),
                 Tables\Columns\TextColumn::make('slug'),
                 Tables\Columns\ImageColumn::make('thumbnail'),
+                Tables\Columns\ToggleColumn::make('is_featured'),
             ])
             ->filters([
                 Tables\Filters\SelectFilter::make('author_id')
