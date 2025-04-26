af2fd27ac005481731684ce07edccb776fc6fffd
ahmadrezapahlevi <zare95752@gmail.com>
2025-04-26 21:09:03 +0700

    update lagi

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Banner;
 use App\Models\News;
+use App\Models\Author;
+
+use Illuminate\Support\Str;
 
 class LandingController extends Controller
 {
@@ -12,7 +15,9 @@ class LandingController extends Controller
     {
       $banners = Banner::all();
       $featureds = News::where('is_featured', true)->get();
+      $news = News::orderBy('created_at', 'desc')->get()->take(4);
+      $authors = Author::all()->take(5);
       
-      return view('pages.landing', compact('banners', 'featureds'));
+      return view('pages.landing', compact('banners', 'featureds', 'news', 'authors'));
     }
 }
